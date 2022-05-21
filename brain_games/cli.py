6b65c0144831a703f8c0d34dbe80c4421071947b
ilitili <94796793+ilitili@users.noreply.github.com>
2022-05-21 18:40:08 +0400

    Delete brain_games directory

--- brain_games/cli.py
+++
@@ -1,9 +0,0 @@
-import prompt
-
-
-def welcome_user():  # function name from step 3 of project
-    """Welcome dialog."""
-    name = prompt.string('May I have your name? ')
-    print('Hello, {}!'.format(name))
-
-
