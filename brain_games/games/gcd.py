3acb078a92089835983c6958896b2f5a32ddb184
ilitili <94796793+ilitili@users.noreply.github.com>
2022-06-14 00:56:57 +0400

    Update gcd.py

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -40,7 +40,7 @@ def gcd(num1, num2):
     """
     while num1 != num2:
         if num1 > num2:
-            num1 -= num2
+            num1 = num1 % num2
         else:
-            num2 -= num1
+            num2 = num2 % num1
     return (num1)
